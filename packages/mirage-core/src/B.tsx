8d9b3362d311c28daa31fde1685ac4f8ae685a13
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-04-02 11:42:20 +0000

    Add reusable button

---
+++ packages/mirage-core/src/B.tsx
@@ -0,0 +1,34 @@
+import React, { ButtonHTMLAttributes, FC, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@mirage/utils";
+
+const variants = cva("bg-red-500 px-4 py-2 rounded", {
+  variants: {
+    variant: {
+      default: "bg-blue-500 rounded-md",
+      primary: "bg-blue-300 rounded-lg",
+    },
+
+    size: {
+      large: "px-8 py-4",
+      small: "px-2 py-1",
+    },
+  },
+
+  defaultVariants: {
+    size: "large",
+    variant: "default",
+  },
+});
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof variants> {}
+
+const B = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, ...props }, ref) => {
+    return <button ref={ref} className={cn(variants({ variant, size, className }))} {...props} />;
+  }
+);
+
+export default B;
--- packages/mirage-utils/src/index.tsx
+++ packages/mirage-utils/src/index.tsx
@@ -1,3 +1,4 @@
 export { toSlug } from "./toSlug";
 export { useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
 export { usePrevious } from "./usePrevious";
+export { cn } from "./classnames";
