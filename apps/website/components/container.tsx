d7778d095a1fd42ceb4464b476b551f99178fb06
4eug <debraheug@gmail.com>
2023-04-04 11:42:04 +0000

    Container

---
+++ apps/website/components/container.tsx
@@ -0,0 +1,53 @@
+import classNames from 'classnames';
+
+export const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={classNames('mx-auto max-w-[120rem] px-8', className)}>
+      {children}
+    </div>
+  );
+};
+
+export const HeaderContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={classNames(
+        'mx-auto px-[2px] py-[16px] lg:px-[92px] lg:py-[32px]',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const FooterContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={classNames(
+        'flex justify-center items-center text-center',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
