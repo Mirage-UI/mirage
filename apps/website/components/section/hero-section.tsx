ee558da1d8e0c02a5fa1a3d8f772d0aa40177cce
4eug <debraheug@gmail.com>
2023-04-04 18:22:02 +0000

    Copy Change

--- apps/website/components/section/hero-section.tsx
+++ apps/website/components/section/hero-section.tsx
@@ -1,25 +1,29 @@
 import { Button } from '../button';
 import { Hero, HeroTitle, HeroSubtitle } from '../hero';
+import { Ico } from '../images/Icons/ico';
+import TimerCard from '../timer-card';
 
 export const HomepageHero = () => (
   <>
     <Hero>
       <Button className="" href="/" variant="secondary" size="medium">
+        <Ico />
         <span>
           {' '}
           Release 2023.05 | <span className="text-primary600">Coming Soon</span>
         </span>
       </Button>
       <HeroTitle className="">
-        The Ultimate Design <br /> System <br /> for Developers and
-        <br />
-        Designers!
+        The Ultimate Design <br /> System for <br /> Devs and Designers!
+        {/* <br />
+        Designers! */}
       </HeroTitle>
       <HeroSubtitle className="">
         Elevate your designs with Mirage's extensive library of
         <br />
         customisable components and styles
       </HeroSubtitle>
+      <TimerCard />
     </Hero>
   </>
 );
