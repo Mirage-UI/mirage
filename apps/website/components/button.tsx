da80352a73d701b2c92faf4bf882664a1ee06e2b
4eug <debraheug@gmail.com>
2023-04-04 11:42:45 +0000

    Button

---
+++ apps/website/components/button.tsx
@@ -0,0 +1,58 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import classNames from 'classnames';
+import Link from 'next/link';
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react';
+
+type ButtonBaseProps = VariantProps<typeof buttonClasses> & {
+  children: React.ReactNode;
+};
+
+interface ButtonAsAnchorProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  href: string;
+}
+
+interface ButtonAsButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  href?: never;
+}
+
+type ButtonProps = ButtonBaseProps &
+  (ButtonAsAnchorProps | ButtonAsButtonProps);
+
+const buttonClasses = cva('relative rounded-[15px] inline-flex items-center', {
+  variants: {
+    variant: {
+      primary: ['bg-primary-gradient hover:text-shadow hover:shadow-primary'],
+      secondary: [
+        'bg-white bg-opacity-10  border-transparent-white backdrop-filter-[12px] hover:bg-opacity-20 transition-colors ease-in',
+        ,
+      ],
+    },
+    size: {
+      small: 'text-sm px-3 h-7',
+      medium: 'text-default px-4 h-8',
+      large: 'text-lg px-6 h-12',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'medium',
+  },
+});
+
+export const Button = ({ children, variant, size, ...props }: ButtonProps) => {
+  const classes = buttonClasses({ variant, size, className: props.className });
+
+  if ('href' in props && props.href !== undefined) {
+    return (
+      <Link {...props} className={classes}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button {...props} className={classes}>
+      {children}
+    </button>
+  );
+};
